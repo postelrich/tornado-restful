d1a1f392c319922982ef4e82ff85f22feae084ea
Richard Postelnik <postelrich@gmail.com>
2020-06-24 21:46:28 -0400

    add desc and classifiers

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
 import versioneer
 from setuptools import setup, find_packages
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 
 setup(
     name='tornado-restful',
@@ -8,13 +11,15 @@ setup(
     version=versioneer.get_version(),
     cmdclass=versioneer.get_cmdclass(),
     description='library for easy restful APIs in tornado',
+    long_description=long_description,
     install_requires=['tornado'],
-    extras_require={
-        'test': ['pytest',
-                 'pytest-spec']
-    },
     author='Richard Postelnik',
     author_email='richard.postelnik@gmail.com',
     url='https://github.com/postelrich/tornado-restful',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: New BSD",
+        "Operating System :: OS Independent",
+    ],
     zip_safe=False
 )
